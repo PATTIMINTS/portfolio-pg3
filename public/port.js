8efce3a893c4505be68b0a50185909ecf4e2137b
Pat Seade <pattimint6@gmail.com>
2019-02-02 22:41:39 -0600

    mongo done

---
+++ public/port.js
@@ -0,0 +1,43 @@
+$(function() {
+  $("#image-One").hover(function() {
+    $(this)
+      .find("#one")
+      .toggle(200);
+  });
+});
+$(function() {
+  $("#image-two").hover(function() {
+    $(this)
+      .find("#last")
+      .toggle(200);
+  });
+});
+$(function() {
+  $("#image-Three").hover(function() {
+    $(this)
+      .find("#three")
+      .toggle(200);
+  });
+});
+$(function() {
+  $("#image-Four").hover(function() {
+    $(this)
+      .find("#four")
+      .toggle(200);
+  });
+});
+$(function() {
+  $("#image-Five").hover(function() {
+    $(this)
+      .find("#five")
+      .toggle(200);
+  });
+});
+
+$(function() {
+  $("#image-Six").hover(function() {
+    $(this)
+      .find("#final")
+      .toggle(200);
+  });
+});
