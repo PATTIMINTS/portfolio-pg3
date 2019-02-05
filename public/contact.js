8e49082dc72e5e8f754872e1bd0c2a86a5eb0415
Pat Seade <pattimint6@gmail.com>
2019-02-05 00:46:33 -0600

    fix

--- public/contact.js
+++ public/contact.js
@@ -13,6 +13,7 @@
 //           data[i]._id +
 //           ">" +
 //           data[i].name +
+
 //           "><span class='dataEmail' data-id=" +
 //           data[i]._id +
 //           ">" +
@@ -20,18 +21,18 @@
 //           "><span class='dataComments' data-id=" +
 //           data[i]._id +
 //           ">" +
-//           data[i].comment +
+//           data[i].comments +
 //           "</span><span class=delete>X</span></p>"
 //       );
 //     }
 //   });
 // }
 
-// Runs the getResults function as soon as the script is executed
+// // Runs the getResults function as soon as the script is executed
 // getResults();
-function handleInput() {
-  // When the #make-new button is clicked
-  $(document).on("click", "#btn");
+
+// When the #make-new button is clicked
+$(document).on("click", "#btn", function() {
   // AJAX POST call to the submit route on the server
   // This will take the data from the form and send it to the server
   $.ajax({
@@ -45,110 +46,107 @@ function handleInput() {
       created: Date.now()
     }
   });
-}
+  //     // If that API call succeeds, add the title and a delete button for the note to the page
+  //     .then(function(data) {
+  //       // Add the title and delete button to the #results section
+  //   $("#results").prepend(
+  //     "<p class='data-entry' data-id=" +
+  //       data._id +
+  //       "><span class='dataTitle' data-id=" +
+  //       data._id +
+  //       ">" +
+  //       data.title +
+  //       "</span><span class=delete>X</span></p>"
+  //   );
+  //   // Clear the note and title inputs on the page
+  //   $("#note").val("");
+  //   $("#title").val("");
+  // });
 
-handleInput();
+  // // When the #clear-all button is pressed
+  // $("#clear-all").on("click", function() {
+  //   // Make an AJAX GET request to delete the notes from the db
+  //   $.ajax({
+  //     type: "GET",
+  //     dataType: "json",
+  //     url: "/clearall",
+  //     // On a successful call, clear the #results section
+  //     success: function(response) {
+  //       $("#results").empty();
+  //     }
+  //   });
+  // });
 
-//     // If that API call succeeds, add the title and a delete button for the note to the page
-//     .then(function(data) {
-//       // Add the title and delete button to the #results section
-//   $("#results").prepend(
-//     "<p class='data-entry' data-id=" +
-//       data._id +
-//       "><span class='dataTitle' data-id=" +
-//       data._id +
-//       ">" +
-//       data.title +
-//       "</span><span class=delete>X</span></p>"
-//   );
-//   // Clear the note and title inputs on the page
-//   $("#note").val("");
-//   $("#title").val("");
-// });
+  // // When user clicks the delete button for a note
+  // $(document).on("click", ".delete", function() {
+  //   // Save the p tag that encloses the button
+  //   var selected = $(this).parent();
+  //   // Make an AJAX GET request to delete the specific note
+  //   // this uses the data-id of the p-tag, which is linked to the specific note
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "/delete/" + selected.attr("data-id"),
 
-// // When the #clear-all button is pressed
-// $("#clear-all").on("click", function() {
-//   // Make an AJAX GET request to delete the notes from the db
-//   $.ajax({
-//     type: "GET",
-//     dataType: "json",
-//     url: "/clearall",
-//     // On a successful call, clear the #results section
-//     success: function(response) {
-//       $("#results").empty();
-//     }
-//   });
-// });
+  //     // On successful call
+  //     success: function(response) {
+  //       // Remove the p-tag from the DOM
+  //       selected.remove();
+  //       // Clear the note and title inputs
+  //       $("#name").val("");
+  //       $("#email").val(""), $("#message").val("");
+  //       // Make sure the #action-button is submit (in case it's update)
+  //       $("#action-button").html("<button id='make-new'>Submit</button>");
+  //     }
+  //   });
+  // });
 
-// // When user clicks the delete button for a note
-// $(document).on("click", ".delete", function() {
-//   // Save the p tag that encloses the button
-//   var selected = $(this).parent();
-//   // Make an AJAX GET request to delete the specific note
-//   // this uses the data-id of the p-tag, which is linked to the specific note
-//   $.ajax({
-//     type: "GET",
-//     url: "/delete/" + selected.attr("data-id"),
+  // // When user click's on note title, show the note, and allow for updates
+  // $(document).on("click", ".dataName", function() {
+  //   // Grab the element
+  //   var selected = $(this);
+  //   // Make an ajax call to find the note
+  //   // This uses the data-id of the p-tag, which is linked to the specific note
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "/find/" + selected.attr("data-id"),
+  //     success: function(data) {
+  //       // Fill the inputs with the data that the ajax call collected
+  //       $("#message").val(data.message);
+  //       $("#name").val(data.name);
+  //       // Make the #action-button an update button, so user can
+  //       // Update the note s/he chooses
+  //       $("#action-button").html(
+  //         "<button id='updater' data-id='" + data._id + "'>Update</button>"
+  //       );
+  //     }
+  //   });
+  // });
 
-//     // On successful call
-//     success: function(response) {
-//       // Remove the p-tag from the DOM
-//       selected.remove();
-//       // Clear the note and title inputs
-//       $("#name").val("");
-//       $("#email").val(""), $("#message").val("");
-//       // Make sure the #action-button is submit (in case it's update)
-//       $("#action-button").html("<button id='make-new'>Submit</button>");
-//     }
-//   });
-// });
-
-// // When user click's on note title, show the note, and allow for updates
-// $(document).on("click", ".dataName", function() {
-//   // Grab the element
-//   var selected = $(this);
-//   // Make an ajax call to find the note
-//   // This uses the data-id of the p-tag, which is linked to the specific note
-//   $.ajax({
-//     type: "GET",
-//     url: "/find/" + selected.attr("data-id"),
-//     success: function(data) {
-//       // Fill the inputs with the data that the ajax call collected
-//       $("#message").val(data.message);
-//       $("#name").val(data.name);
-//       // Make the #action-button an update button, so user can
-//       // Update the note s/he chooses
-//       $("#action-button").html(
-//         "<button id='updater' data-id='" + data._id + "'>Update</button>"
-//       );
-//     }
-//   });
-// });
-
-// // When user click's update button, update the specific note
-// $(document).on("click", "#updater", function() {
-//   // Save the selected element
-//   var selected = $(this);
-//   // Make an AJAX POST request
-//   // This uses the data-id of the update button,
-//   // which is linked to the specific note title
-//   // that the user clicked before
-//   $.ajax({
-//     type: "POST",
-//     url: "/update/" + selected.attr("data-id"),
-//     dataType: "json",
-//     data: {
-//       title: $("#title").val(),
-//       note: $("#note").val()
-//     },
-//     // On successful call
-//     success: function(data) {
-//       // Clear the inputs
-//       $("#note").val("");
-//       $("#title").val("");
-//       // Revert action button to submit
-//       $("#action-button").html("<button id='make-new'>Submit</button>");
-//       // Grab the results from the db again, to populate the DOM
-//       getResults();
-//   }
-// });
+  // // When user click's update button, update the specific note
+  // $(document).on("click", "#updater", function() {
+  //   // Save the selected element
+  //   var selected = $(this);
+  //   // Make an AJAX POST request
+  //   // This uses the data-id of the update button,
+  //   // which is linked to the specific note title
+  //   // that the user clicked before
+  //   $.ajax({
+  //     type: "POST",
+  //     url: "/update/" + selected.attr("data-id"),
+  //     dataType: "json",
+  //     data: {
+  //       title: $("#title").val(),
+  //       note: $("#note").val()
+  //     },
+  //     // On successful call
+  //     success: function(data) {
+  //       // Clear the inputs
+  //       $("#note").val("");
+  //       $("#title").val("");
+  //       // Revert action button to submit
+  //       $("#action-button").html("<button id='make-new'>Submit</button>");
+  //       // Grab the results from the db again, to populate the DOM
+  //       getResults();
+  //   }
+  // });
+});
